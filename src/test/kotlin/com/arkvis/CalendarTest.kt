2b21111538d9d309481390cfcdbee262a61eb13b
lodenrogue <phyzix5761@gmail.com>
2022-08-28 13:24:07 -0400

    Added adding multiple periods

--- src/test/kotlin/com/arkvis/CalendarTest.kt
+++ src/test/kotlin/com/arkvis/CalendarTest.kt
@@ -3,7 +3,6 @@ package com.arkvis
 import com.arkvis.orchid.OvulationPredictor
 import com.arkvis.orchid.PeriodCalendar
 import com.arkvis.orchid.PeriodPredictor
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeEach
@@ -32,5 +31,24 @@ class CalendarTest {
 
         val retrievedDay = periodCalendar.getDay(date)
         assertNotNull(retrievedDay.period)
+    }
+
+    @Test
+    fun should_returnMultipleDaysWithPeriod_when_multipleDaysAreAddedToCalendar() {
+        val date1 = LocalDate.now()
+        val date2 = date1.plusDays(1)
+        val date3 = date1.plusDays(2)
+
+        periodCalendar.addPeriod(date1)
+        periodCalendar.addPeriod(date2)
+        periodCalendar.addPeriod(date3)
+
+        val day1 = periodCalendar.getDay(date1)
+        val day2 = periodCalendar.getDay(date2)
+        val day3 = periodCalendar.getDay(date3)
+
+        assertNotNull(day1.period)
+        assertNotNull(day2.period)
+        assertNotNull(day3.period)
     }
 }
